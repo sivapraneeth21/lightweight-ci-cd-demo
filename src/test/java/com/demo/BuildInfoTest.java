03ebfa478de9ab4d6fac60f6950ad76129888fdd
sivapraneeth21 <90759415+sivapraneeth21@users.noreply.github.com>
2026-09-18 14:18:05 +0530

    Update application name in BuildInfoTest

--- src/test/java/com/demo/BuildInfoTest.java
+++ src/test/java/com/demo/BuildInfoTest.java
@@ -4,6 +4,6 @@ import org.junit.jupiter.api.Test;
 class BuildInfoTest {
   @Test
   void applicationNameIsCorrect() {
-    assertEquals("Lightweight CI/CD", BuildInfo.applicationName());
+    assertEquals("Lightweight CI/CD Demo", BuildInfo.applicationName());
   }
 }
